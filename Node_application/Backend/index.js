e5f4b1b4015d1b9cd98edc865e5239e54060843c
Mads trætteberg <Mads.traetteberg@gmail.com>
2021-10-15 13:28:05 +0200

    Jobbet videre med backenden av node oppgaven

--- Node_application/Backend/index.js
+++ Node_application/Backend/index.js
@@ -6,6 +6,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'root',
+  database:'dromtorp'
+})
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields){
+  if(error) throw error;
+  console.log('The solution is: ', results[0].solution)
+)};
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
