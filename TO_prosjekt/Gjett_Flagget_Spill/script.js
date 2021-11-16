10dd440576bf3a71e9106a1c8c75e67f296ec532
Mads trætteberg <Mads.traetteberg@gmail.com>
2021-11-16 14:41:22 +0100

    Jeg har jobbet videre med javascript filen som inneholder mye av beck-end delen av spillet. Jeg jobber med å ha en start-knapp man kan trykke på, for å få opp det første spørsmålet og svaralternativene. Jeg slet litt med å få det til, men jeg skal prøve mer når jeg får tid.

--- TO_prosjekt/Gjett_Flagget_Spill/script.js
+++ TO_prosjekt/Gjett_Flagget_Spill/script.js
@@ -1,15 +1,38 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElements = document.getElementById('question-container')
+let shuffledQuestions, currentQuestionIndex
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
 startButton.addEventListener('click', startGame)
 
 function startGame(){
   console.log('Started')
-}
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElements.classList.remove('hide')
+  setNextQuestion()
+  }
 
 function setNextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
+function showQuestion(question) {
+  questionElement.innterText = question.question
 }
 
 function selectAnswer() {
 
 }
+
+const question = [
+  {
+    question: 'Hvilket flagg er dette?',
+    answers: [
+      {text: 'Norge', correct: true},
+      {text: 'Sverige', correct: false}
+    ]
+  }
+]
