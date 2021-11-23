796cc22bec3c0e196cf59c0b9343b9b795e6fa9a
Mads trætteberg <Mads.traetteberg@gmail.com>
2021-11-23 14:46:47 +0100

    Jobbet videre med flagg-quizen.

--- TO_prosjekt/Gjett_Flagget_Spill/script.js
+++ TO_prosjekt/Gjett_Flagget_Spill/script.js
@@ -1,30 +1,59 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElements = document.getElementById('question-container')
-let shuffledQuestions, currentQuestionIndex
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame(){
   console.log('Started')
   startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  shuffledQuestions = question.sort(() => Math.random() - .5)
   currentQuestionIndex = 0
   questionContainerElements.classList.remove('hide')
   setNextQuestion()
   }
 
 function setNextQuestion() {
+  resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
-  questionElement.innterText = question.question
+
 }
 
-function selectAnswer() {
+function resetState() {
+  while (answerButtonsElement.firstCild) {
+    answerButtonsElement.removeChild
+    (answerButtonsElement.firstCild)
+  }
+}
 
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  nextButton.classList.remove('hide')
+}
+
+function setStatusClass(element, correct){
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
 const question = [
