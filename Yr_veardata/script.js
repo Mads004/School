1214172605f209c30984726c02f85034bb7b5edd
Mads trætteberg <Mads.traetteberg@gmail.com>
2021-12-02 15:37:12 +0100

    Jobbet med et nytt prosjekt hvor det skal vise været i blant annet Ski, Oslo, Bergen, og Trondheim

---
+++ Yr_veardata/script.js
@@ -0,0 +1,7 @@
+fetch('https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=-16.516667&lon=-68.166667&altitude=4150')
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+function getElementById('1'){
+  
+}
