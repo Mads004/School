f7fb76f3498f401b7ea8e63c0754fe1d6ea618e9
Mads trætteberg <Mads.traetteberg@gmail.com>
2021-12-03 13:27:55 +0100

    Jeg har jobbet videre med javascripten og litt inne på html og CSS. Jeg har fått til at når man trykker på knappene, så forsvinner 'tempature1' og så videre, men jeg får ikke til å erstatte den teksten med dataen enda.

--- Yr_veardata/script.js
+++ Yr_veardata/script.js
@@ -1,7 +1,32 @@
-fetch('https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=60.1&lon=9.58&altitude=130')
-  .then(response => response.json())
-  .then(data => console.log(data.properties.timeseries[0].data.instant.details));
 
-  document.getElementById("btn1").onclick = function() {
+document.getElementById("btn1").onclick = function() {
     console.log('Været for Ski');
-  }
+    document.getElementById('1').style.display = "none";
+    fetch('https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=60.1&lon=9.58&altitude=130')
+    .then(response => response.json())
+    .then(data => console.log(data.properties.timeseries[0].data.instant.details));
+}
+
+document.getElementById("btn2").onclick = function() {
+    console.log('Været for Oslo');
+    document.getElementById('2').style.display = "none";
+    fetch('https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=60.1&lon=9.58&altitude=130')
+    .then(response => response.json())
+    .then(data => console.log(data.properties.timeseries[0].data.instant.details));
+}
+
+document.getElementById("btn3").onclick = function() {
+    console.log('Været for Bergen');
+    document.getElementById('3').style.display = "none";
+    fetch('https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=60.1&lon=9.58&altitude=130')
+    .then(response => response.json())
+    .then(data => console.log(data.properties.timeseries[0].data.instant.details));
+}
+
+document.getElementById("btn4").onclick = function() {
+    console.log('Været for Trondheim');
+    document.getElementById('4').style.display = "none";
+    fetch('https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=60.1&lon=9.58&altitude=130')
+    .then(response => response.json())
+    .then(data => console.log(data.properties.timeseries[0].data.instant.details));
+}
