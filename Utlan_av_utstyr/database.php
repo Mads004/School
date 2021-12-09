febd1e2e43c7c2dcb07181cbd573d8634a471e04
Mads trætteberg <Mads.traetteberg@gmail.com>
2021-12-09 15:33:20 +0100

    I dag har jeg jobbet videre med nettsiden. Jeg slet med griden i går, men jeg fikk fikset det i dag. Det var noe feil med divene som gjorde at alle divene havnet i en grid rute, isteden for de 10 forskjellige gridsa. Ellers, så la jeg til infoteksten og skygge på boksen. Jeg har også begynt på en knapp, som skal vise alle tingene brukeren har markert for å se hvor mange av den tingen som er ledig for å bli lånt. Jeg skal jobbe mer med php-filen og databasen i morgen.

---
+++ Utlan_av_utstyr/database.php
@@ -0,0 +1,25 @@
+<?php
+  // connect to database
+  $conn = mysqli_connect('localhost', 'mdt', 'Skole123*', 'utlan');
+
+  // check connection
+  if(!$conn){
+    echo 'Connection error: ' . mysqli_connect_error();
+  }
+
+  // write query for all utlan
+  $sql = 'SELECT PC, lader, ruter, switch FROM utstyr';
+
+  // make query & get result
+  $result = mysqli_query($conn, $sql);
+
+  // fetch the resulting rows as an array
+  $utstyr = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  mysqli_free_result($result);
+
+  // close connection
+  mysqli_close($conn)
+
+  print_r($utstyr);
+ ?>
