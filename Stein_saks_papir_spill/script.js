323fea7ad6df986b63742acc5ec33eec58280694
Mads trætteberg <Mads.traetteberg@gmail.com>
2022-04-19 14:37:44 +0200

    Fortsatt videre med knapp til spillet som skal starte spillet. Jeg har ikke fått den til å fungere helt enda. Jeg har også funnet en bra Javascript-kode for stein saks papir, som er spiller mot robot.

---
+++ Stein_saks_papir_spill/script.js
@@ -0,0 +1,81 @@
+const options = document.querySelectorAll(".options");
+let pScore = 0;
+let cScore = 0;
+
+gameButton.addEventListener('click', game)
+
+function game(){
+console.log('Started')
+document.getElementById('game').style.display = "none";
+}
+
+options.forEach((option) => {
+  option.addEventListener("click", function () {
+    const pInput = this.textContent;
+
+    const cOptions = ["Rock", "Paper", "Scissors"];
+    const cInput = cOptions[Math.floor(Math.random() * 3)];
+
+    compareInputs(pInput, cInput);
+    updateScore();
+    if (checkWinner()) {
+      pScore = cScore = 0;
+      updateScore();
+    }
+  });
+});
+
+function compareInputs(pInput, cInput) {
+  const currentMatch = `${pInput} vs ${cInput}`;
+  if (pInput === cInput) {
+    alert(`${currentMatch} is a Tie`);
+    return;
+  }
+
+if (pInput === "Rock") {
+  if (cInput === "Scissors") {
+    alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
+    }
+  }
+//Check for Paper
+else if (pInput === "Paper") {
+  if (cInput === "Rock") {
+    alert(`${currentMatch} = You Win`);
+    pScore++;
+  } else {
+    alert(`${currentMatch} = Computer Wins`);
+    cScore++;
+  }
+}
+//Check for Scissors
+else {
+  if (cInput === "Paper") {
+    alert(`${currentMatch} = You Win`);
+    pScore++;
+  } else {
+    alert(`${currentMatch} = Computer Wins`);
+    cScore++;
+  }
+}
+}
+
+function updateScore() {
+  document.getElementById("p-score").textContent = pScore;
+  document.getElementById("c-score").textContent = cScore;
+}
+
+function checkWinner() {
+  if (pScore === 5 || cScore === 5) {
+    const winner =
+      pScore === 5
+        ? "You win the game! Congratulations!"
+        : "Computer wins the game! Try again next time!";
+    alert(winner);
+    return true;
+  }
+  return false;
+}
