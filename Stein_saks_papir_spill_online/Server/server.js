bd4cfa00e333b2752cb7f81e0259b4e7f96c9469
Mads trætteberg <Mads.traetteberg@gmail.com>
2022-05-10 14:41:07 +0200

    Jeg er jobbet videre med multiplayer på Stein-saks-papir spillet

--- Stein_saks_papir_spill_online/Server/server.js
+++ Stein_saks_papir_spill_online/Server/server.js
@@ -1,8 +1,15 @@
 const app = require('express')();
 const http = require('http').Server(app);
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', function(req, res) {
-   res.sendfile('index.html');
+   res.sendfile('chat.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
 });
 
 http.listen(3000, function() {
