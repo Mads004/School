ae7d72d0d16e85103a5ebedfedb39b39a8b8ac2c
Mads trætteberg <Mads.traetteberg@gmail.com>
2022-05-12 15:36:42 +0200

    Jeg har jobbet videre på oppgaven

--- Stein_saks_papir_spill_online/Server/server.js
+++ Stein_saks_papir_spill_online/Server/server.js
@@ -1,17 +1,17 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const express = require('express')
+const app = express()
+const port = 3000
+const server = require('http').createServer();
+const io = require('socket.io')(server);
 
-app.get('/', function(req, res) {
-   res.sendfile('chat.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-http.listen(3000, function() {
-   console.log('listening on *:3000');
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
