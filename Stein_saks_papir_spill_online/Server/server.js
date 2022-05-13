819891ab305fdb2e8903d15280791017322e6e88
Mads trætteberg <Mads.traetteberg@gmail.com>
2022-05-13 13:26:53 +0200

    Jeg har fortsatt på oppgaven

--- Stein_saks_papir_spill_online/Server/server.js
+++ Stein_saks_papir_spill_online/Server/server.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const server = require('http').createServer();
-const io = require('socket.io')(server);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+   res.sendfile('chat-example/chat.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+http.listen(3000, function() {
+   console.log('listening on *:3000');
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+   console.log('A user connected');
+
+//Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
 });
