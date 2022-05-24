ab367b69876617e798319eaf8118b5ebb6c8b6ab
Mads trætteberg <Mads.traetteberg@gmail.com>
2022-05-24 14:43:02 +0200

    Jeg har fortsatt videre på vitseappen, og gjort at man får opp en vits i konsollen.

--- VitseApp/script.js
+++ VitseApp/script.js
@@ -1,8 +1,16 @@
+fetch('https://v2.jokeapi.dev/joke/Dark')
+.then(response => response.json())
+.then(data => console.log(data));
+
 function showText() {
   console.log("Found InnerText")
   document.getElementById('vits').innerText = "En vits";
 }
 
 function updateValue() {
-  onclick()
+   document.getElementById('vits').display = "none";
+}
+
+if(document.getElementById('newVits').clicked == true){
+ console.log("button was clicked");
 }
