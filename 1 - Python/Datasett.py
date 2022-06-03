b624518261916b15577a259cba0c794f9513a8fe
Mads trætteberg <Mads.traetteberg@gmail.com>
2022-06-03 13:25:16 +0200

    Jobbet med python oppgaver

---
+++ 1 - Python/Datasett.py
@@ -0,0 +1,7 @@
+#5.2.1, A) Kommandoen transpose() får rader til å bli kolonner, og motsatt.
+
+#5.2.1, B) Hvis man skriver transpose(), så vil tabellen bytte mellom rader og kolonner.
+
+#5.2.1, C) Ja, tabellen har det samme innholdet selv om rader har blitt til kolonner og omvendt.
+
+
